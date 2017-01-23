2081fc92936fab1e56125fd21d88f6a46ad4c569
radu <radu.berdan@gmail.com>
2017-01-23 15:46:28 +0000

    Minor modifications relating to VirtualArc

--- Globals/GlobalVars.py
+++ Globals/GlobalVars.py
@@ -33,8 +33,8 @@ N=32
 
 Vread=0.5
 readCycles=50
-#ser=serial.Serial() # serial object
-ser=virtualarc.virtualArC([])
+ser=serial.Serial() # serial object
+#ser=virtualarc.virtualArC([])
 
 Mhistory=[[[] for bit in range(33)] for word in range(33)] 	# Main data container
 # arrays of floats updated each time Mhistory is updated to be used for data display
