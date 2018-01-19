7f38891b8aff27f4eb7dbc79a7d48f00706d91ea
Spyros Stathopoulos <ss2y15@soton.ac.uk>
2018-01-19 16:39:17 +0000

    Initial commit of virtualArc with parametric model

--- Globals/GlobalFunctions.py
+++ Globals/GlobalFunctions.py
@@ -13,17 +13,18 @@ from PyQt4 import QtGui
 from time import sleep
 import numpy as np
 import collections
+from virtualArC import virtualarc
 
 
 ###########################################
 # Signal Routers
 ###########################################
-class cbAntenna(QObject): 		# class which routes signals (socket) where instantiated
-    selectDeviceSignal=pyqtSignal(int, int) 	# used for highlighting/deselecting devices on the crossbar panel
+class cbAntenna(QObject):       # class which routes signals (socket) where instantiated
+    selectDeviceSignal=pyqtSignal(int, int)     # used for highlighting/deselecting devices on the crossbar panel
     deselectOld=pyqtSignal()
     redraw=pyqtSignal()
     reformat=pyqtSignal()
-    							# and signals updating the current select device
+                                # and signals updating the current select device
     recolor=pyqtSignal(float, int, int)
 
     def __init__(self):
@@ -35,21 +36,21 @@ cbAntenna=cbAntenna()
 
 
 class displayUpdate(QObject):
-	updateSignal_short=pyqtSignal()
-	updateSignal=pyqtSignal(int, int, int, 	int, 	int)
-						#   w,   b,   type, points, slider
-	updateLog=pyqtSignal(int)
+    updateSignal_short=pyqtSignal()
+    updateSignal=pyqtSignal(int, int, int,  int,    int)
+                        #   w,   b,   type, points, slider
+    updateLog=pyqtSignal(int)
 
-	def __init__(self):
-		super(displayUpdate,self).__init__()
+    def __init__(self):
+        super(displayUpdate,self).__init__()
 
-	def cast(self):
-		self.updateSignal_short.emit()
+    def cast(self):
+        self.updateSignal_short.emit()
 displayUpdate=displayUpdate()
 
 
-class historyTreeAntenna(QObject): 		# class which routes signals (socket) where instantiated
-    updateTree=pyqtSignal(int,int) 	# used for signaling the device history tree list to update its contents							
+class historyTreeAntenna(QObject):      # class which routes signals (socket) where instantiated
+    updateTree=pyqtSignal(int,int)  # used for signaling the device history tree list to update its contents
     updateTree_short=pyqtSignal()
     clearTree=pyqtSignal()
     changeSessionName=pyqtSignal()
@@ -60,37 +61,37 @@ historyTreeAntenna=historyTreeAntenna()
 
 
 class interfaceAntenna(QObject):
-	disable=pyqtSignal(bool)
-	reformat=pyqtSignal()
-	changeArcStatus=pyqtSignal(str)
-	changeSessionMode=pyqtSignal(str)
-	updateHW=pyqtSignal()
-	lastDisplaySignal=pyqtSignal()
+    disable=pyqtSignal(bool)
+    reformat=pyqtSignal()
+    changeArcStatus=pyqtSignal(str)
+    changeSessionMode=pyqtSignal(str)
+    updateHW=pyqtSignal()
+    lastDisplaySignal=pyqtSignal()
 
-	globalDisable=False
+    globalDisable=False
 
-	def __init__(self):
-		super(interfaceAntenna,self).__init__()
+    def __init__(self):
+        super(interfaceAntenna,self).__init__()
 
-	def wakeUp(self):
-		g.waitCondition.wakeAll()
-		#print " --> waitCondition wakedAll"
-	def cast(self, value):
-		# if value==False:
-		# 	g.waitCondition.wakeAll()
-		# 	print " --> waitCondition wakedAll"
-		if self.globalDisable==False:
-			self.disable.emit(value)
-			#sleep(0.1)
-			self.lastDisplaySignal.emit()
-			
+    def wakeUp(self):
+        g.waitCondition.wakeAll()
+        #print " --> waitCondition wakedAll"
+    def cast(self, value):
+        # if value==False:
+        #   g.waitCondition.wakeAll()
+        #   print " --> waitCondition wakedAll"
+        if self.globalDisable==False:
+            self.disable.emit(value)
+            #sleep(0.1)
+            self.lastDisplaySignal.emit()
 
-	def castArcStatus(self, value):
-		if self.globalDisable==False:
-			self.changeArcStatus.emit(value)		
 
-	def toggleGlobalDisable(self, value):
-		self.globalDisable=value
+    def castArcStatus(self, value):
+        if self.globalDisable==False:
+            self.changeArcStatus.emit(value)
+
+    def toggleGlobalDisable(self, value):
+        self.globalDisable=value
 interfaceAntenna=interfaceAntenna()
 
 # updates the range of devices for each pulsing script thread
@@ -99,59 +100,59 @@ interfaceAntenna=interfaceAntenna()
 ###########################################
 
 class SAantenna(QObject):
-	disable=pyqtSignal(int, int)
-	enable=pyqtSignal(int, int)
+    disable=pyqtSignal(int, int)
+    enable=pyqtSignal(int, int)
 
-	def __init__(self):
-		super(SAantenna,self).__init__()
+    def __init__(self):
+        super(SAantenna,self).__init__()
 SAantenna=SAantenna()
 
 ###########################################
 # Update history function
 ###########################################
 def updateHistory(w,b,m,a,pw,tag):
-	readTag='R'+str(g.readOption)
-	if g.sessionMode==1:
-		g.Mnow=m/2
-	else:
-		g.Mnow=m
-	g.Mhistory[w][b].append([g.Mnow,a,pw,tag,readTag,g.Vread])
-	
+    readTag='R'+str(g.readOption)
+    if g.sessionMode==1:
+        g.Mnow=m/2
+    else:
+        g.Mnow=m
+    g.Mhistory[w][b].append([g.Mnow,a,pw,tag,readTag,g.Vread])
 
-	g.w=w
-	g.b=b
-	cbAntenna.recolor.emit(m,w,b)
+
+    g.w=w
+    g.b=b
+    cbAntenna.recolor.emit(m,w,b)
 
 def updateHistory_CT(w,b,m,a,pw,tag):
-	readTag='R2'
-	#print "received: ", m, a
-	
-	if g.sessionMode==1:
-		g.Mnow=m/2
-	else:
-		g.Mnow=m
+    readTag='R2'
+    #print "received: ", m, a
 
-	g.Mhistory[w][b].append([g.Mnow,a,pw,tag,readTag,a])
+    if g.sessionMode==1:
+        g.Mnow=m/2
+    else:
+        g.Mnow=m
 
-	g.w=w
-	g.b=b
-	cbAntenna.recolor.emit(m,w,b)
+    g.Mhistory[w][b].append([g.Mnow,a,pw,tag,readTag,a])
+
+    g.w=w
+    g.b=b
+    cbAntenna.recolor.emit(m,w,b)
 
 
 def updateHistory_short(m,a,pw,tag):
-	readTag='R'+str(g.readOption)
-	if g.sessionMode==1:
-		g.Mnow=m/2
-	else:
-		g.Mnow=m
-	g.Mhistory[g.w][g.b].append([g.Mnow,a,pw,tag,readTag, g.Vread])
-	#g.MList[g.w][g.b]=np.append(g.MList[g.w][g.b],m)
-	#g.PList[g.w][g.b]=np.append(g.PList[g.w][g.b],0)
-	#g.PList[g.w][g.b]=np.append(g.PList[g.w][g.b],a)
-	#g.PList[g.w][g.b]=np.append(g.PList[g.w][g.b],0)
-	#g.PMarkerList[g.w][g.b]=np.append(g.PMarkerList[g.w][g.b],a)
-	#g.PWList[g.w][g.b]=np.append(g.PWList[g.w][g.b],pw)
-	cbAntenna.recolor.emit(m,g.w,g.b)
+    readTag='R'+str(g.readOption)
+    if g.sessionMode==1:
+        g.Mnow=m/2
+    else:
+        g.Mnow=m
+    g.Mhistory[g.w][g.b].append([g.Mnow,a,pw,tag,readTag, g.Vread])
+    #g.MList[g.w][g.b]=np.append(g.MList[g.w][g.b],m)
+    #g.PList[g.w][g.b]=np.append(g.PList[g.w][g.b],0)
+    #g.PList[g.w][g.b]=np.append(g.PList[g.w][g.b],a)
+    #g.PList[g.w][g.b]=np.append(g.PList[g.w][g.b],0)
+    #g.PMarkerList[g.w][g.b]=np.append(g.PMarkerList[g.w][g.b],a)
+    #g.PWList[g.w][g.b]=np.append(g.PWList[g.w][g.b],pw)
+    cbAntenna.recolor.emit(m,g.w,g.b)
 
 
 def writeDelimitedData(data, dest, delimiter="\t"):
@@ -177,34 +178,32 @@ def saveFuncToFilename(func, title="", parent=None):
 ###########################################
 
 class hoverAntenna(QObject):
-	displayHoverPanel=pyqtSignal(int, int, int, int, int, int)
-	hideHoverPanel=pyqtSignal()
+    displayHoverPanel=pyqtSignal(int, int, int, int, int, int)
+    hideHoverPanel=pyqtSignal()
 
-	def __init__(self):
-		super(hoverAntenna,self).__init__()
+    def __init__(self):
+        super(hoverAntenna,self).__init__()
 
 hoverAntenna=hoverAntenna()
 
 class addressAntenna(QObject):
-	def __init__(self):
-		super(addressAntenna,self).__init__()
+    def __init__(self):
+        super(addressAntenna,self).__init__()
 
-	def update(self, w,b):
-		g.w,g.b=w,b
-		cbAntenna.selectDeviceSignal.emit(w, b)
+    def update(self, w,b):
+        g.w,g.b=w,b
+        cbAntenna.selectDeviceSignal.emit(w, b)
 addressAntenna=addressAntenna()
 
 dataBuffer=[]
 
 def getFloats(n):
-	while g.ser.inWaiting()<n*4:
-		pass
-	values=g.ser.read(size=n*4)	# read n * 4 bits of data (n floats) from the input serial
-	extracted=np.frombuffer(buffer(values), dtype=np.float32)
-	return extracted	# returns a list of these floats
-
-
-
-
-
+    if not isinstance(g.ser, virtualarc.virtualArC):
+        while g.ser.inWaiting()<n*4:
+            pass
+        values=g.ser.read(size=n*4) # read n * 4 bits of data (n floats) from the input serial
+        extracted=np.frombuffer(buffer(values), dtype=np.float32)
+    else:
+        extracted = g.ser.read(n)
+    return extracted    # returns a list of these floats
 
---
+++ virtualArC/parametric_device.py
@@ -0,0 +1,47 @@
+import numpy as np
+import collections
+
+class ParametricDevice(object):
+
+    def __init__(self, Ap, An, a0p, a1p, a0n, a1n, tp, tn):
+
+        super(ParametricDevice, self).__init__()
+        self.Ap = Ap
+        self.An = An
+        self.a0p = a0p
+        self.a1p = a1p
+        self.a0n = a0n
+        self.a1n = a1n
+        self.tp = tp
+        self.tn = tn
+
+    def initialise(self, Rinit):
+        self.Rmem = Rinit
+
+    def hstep(self, param):
+        return np.piecewise(param, [ param <= 0, param > 0 ], [0, 1])
+
+    def r_V(self, V):
+        if V > 0:
+            return self.a0p + self.a1p*V
+        else:
+            return self.a0n + self.a1n*V
+
+    def f_V(self, R, V):
+        if V > 0:
+            return self.hstep(self.r_V(V) - R)*np.power((self.r_V(V) - R), 2)
+        else:
+            return self.hstep(R - self.r_V(V))*np.power((R - self.r_V(V)), 2)
+
+    def s_V(self, V):
+        if V > 0:
+            return self.Ap * (-1 + np.exp(np.abs(V)/self.tp))
+        else:
+            return self.An * (-1 + np.exp(np.abs(V)/self.tn))
+
+    def step_dt(self, Vm, dt):
+
+        dR = self.s_V(Vm) * self.f_V(self.Rmem, Vm) * dt
+
+        self.Rmem = self.Rmem + dR
+
