722f45927dfca104c45dcfd3fb253724ca5d1689
Spyros Stathopoulos <ss2y15@soton.ac.uk>
2019-10-10 18:56:44 +0100

    [genui] Check mtimes before generating python classes from .ui files

--- genui.py
+++ genui.py
@@ -2,13 +2,14 @@
 
 import glob
 import os
+from datetime import datetime
 import os.path
 import sys
 
-from PyQt5.uic import compileUi
+import PyQt5.uic as uic
 
 
-def doCompile(src, dst):
+def compileUi(src, dst):
     """
     Compile UI file `src` into the python file `dst`. This is similar to
     what the pyuic script is doing but with some predefined values.
@@ -32,10 +33,10 @@ def doCompile(src, dst):
             out = open(dst, 'wt')
 
     # see the docstring of PyQt5.uic.compileUi for more!
-    compileUi(src, out, execute=False, indent=4)
+    uic.compileUi(src, out, execute=False, indent=4)
 
 
-def main():
+def main(force=False):
 
     # Find the current working directory (ie. the folder this script resides
     # in). Then find out where the UI files are stored and setup the output
@@ -58,12 +59,24 @@ def main():
         # target filename is the same as source with the .py suffix instead
         # of .ui.
         fname = os.path.splitext(os.path.basename(ui))[0]
+        ui_mtime = datetime.fromtimestamp(os.stat(ui).st_mtime)
+
         target = os.path.join(outdir, "%s.py" % fname)
 
-        print("[UIC] Generating %s " % os.path.basename(target), \
-            file=sys.stderr)
+        if os.path.exists(target):
+            target_mtime = datetime.fromtimestamp(os.stat(target).st_mtime)
+        else:
+            target_mtime = datetime.fromtimestamp(0)
 
-        doCompile(ui, target)
+        if ui_mtime > target_mtime or force:
+            print("[UIC] Generating %s " % os.path.basename(target), \
+                file=sys.stderr)
+        else:
+            print("[UIC] %s is up to date" % os.path.basename(target), \
+                file=sys.stderr)
+
+        compileUi(ui, target)
+
 
 if __name__ == "__main__":
     main()
