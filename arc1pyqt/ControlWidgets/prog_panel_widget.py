64ac00d423f76ac904ca1365a80d79c43a52f1b4
Spyros Stathopoulos <ss2y15@soton.ac.uk>
2020-11-23 16:09:44 +0000

    Add support for saving/loading parameters of individual panels
    Piggybacking on the parameter extraction and loading mechanism introduced
    by previous commits two new buttons have been added to `ProgPanelWidget`
    that provide a way to save and load individual parameters of panels.
    Users can save their measurement setup into a file and recall them later
    instead of typing everything again.

--- arc1pyqt/ControlWidgets/prog_panel_widget.py
+++ arc1pyqt/ControlWidgets/prog_panel_widget.py
@@ -10,7 +10,7 @@
 import sys
 import os
 import importlib
-import pkgutil
+import json
 from PyQt5 import QtGui, QtCore, QtWidgets
 
 from .. import ProgPanels
@@ -33,9 +33,6 @@ class ProgPanelWidget(QtWidgets.QWidget):
 
         hbox_1=QtWidgets.QHBoxLayout()
 
-        label_panels = QtWidgets.QLabel('Panels:')
-        label_panels.setMaximumWidth(40)
-
         self.prog_panelList = QtWidgets.QComboBox()
         self.prog_panelList.setStyleSheet(styles.comboStyle)
         self.prog_panelList.setMinimumWidth(150*APP.scalingFactor)
@@ -47,24 +44,42 @@ class ProgPanelWidget(QtWidgets.QWidget):
             if mod.toplevel is not None:
                 self.prog_panelList.addItem(mod.name, mod)
 
-        boldFont=QtGui.QFont("FontFamily")
+        boldFont=QtGui.QFont()
         boldFont.setBold(True)
-        self.prog_panelList.setItemData(self.prog_panelList.findText("SuperMode"), boldFont, QtCore.Qt.FontRole)
+        self.prog_panelList.setItemData(self.prog_panelList.findText("SuperMode"),
+            boldFont, QtCore.Qt.FontRole)
 
         self.push_add=QtWidgets.QPushButton('Add')
         self.push_add.setStyleSheet(styles.btnStyle2)
+        self.push_add.setToolTip('Add a new panel from list')
         self.push_add.clicked.connect(self.addPanel)
 
         self.push_remove=QtWidgets.QPushButton('Remove')
         self.push_remove.setStyleSheet(styles.btnStyle2)
+        self.push_remove.setToolTip('Remove current panel')
         self.push_remove.clicked.connect(self.removePanel)
 
-        self.tabFrame=QtWidgets.QTabWidget()
+        self.push_save=QtWidgets.QPushButton('Save')
+        self.push_save.setStyleSheet(styles.btnStyle2)
+        self.push_save.clicked.connect(self.savePanel)
+        self.push_save.setToolTip('Save active panel parameters')
+        sp = self.push_save.sizePolicy()
+        sp.setRetainSizeWhenHidden(True)
+        self.push_save.setSizePolicy(sp)
 
-        hbox_1.addWidget(label_panels)
-        hbox_1.addWidget(self.prog_panelList)
-        hbox_1.addWidget(self.push_add)
-        hbox_1.addWidget(self.push_remove)
+        self.push_load=QtWidgets.QPushButton('Load')
+        self.push_load.setStyleSheet(styles.btnStyle2)
+        self.push_load.setToolTip('Load a panel from file')
+        self.push_load.clicked.connect(self.loadPanel)
+
+        self.tabFrame = QtWidgets.QTabWidget()
+        self.tabFrame.currentChanged.connect(self.tabChanged)
+
+        hbox_1.addWidget(self.prog_panelList, 2)
+        hbox_1.addWidget(self.push_add, 1)
+        hbox_1.addWidget(self.push_remove, 1)
+        hbox_1.addWidget(self.push_save, 1)
+        hbox_1.addWidget(self.push_load, 1)
 
         mainLayout.addLayout(hbox_1)
         mainLayout.addWidget(self.tabFrame)
@@ -82,6 +97,60 @@ class ProgPanelWidget(QtWidgets.QWidget):
         widget = topKlass()
         self.tabFrame.addTab(widget, mod.name)
         self.tabFrame.setCurrentWidget(widget)
+
+    def tabChanged(self, *args):
+        wdg = self.tabFrame.currentWidget()
+
+        # SuperMode provides separate facilty for saving data
+        self.push_save.setVisible(not
+            wdg.__class__.__module__.startswith(
+            'arc1pyqt.ProgPanels.SuperMode'))
+
+    def savePanel(self):
+        wdg = self.tabFrame.currentWidget()
+
+        try:
+            saveFileName = QtWidgets.QFileDialog.getSaveFileName(self,
+                'Save panel parameters', '', 'Panel data (*.json)')[0]
+        except IndexError:
+            return
+
+        if saveFileName and hasattr(wdg, 'extractPanelData'):
+            fullmodname = ".".join([
+                wdg.__class__.__module__,
+                wdg.__class__.__name__])
+
+            with open(saveFileName, 'w') as f:
+                json.dump([fullmodname, wdg.extractPanelData()], f)
+
+    def loadPanel(self):
+        try:
+            panelDataFileName = QtWidgets.QFileDialog.getOpenFileName(self,
+                'Open panel from file...', '', 'Panel data (*.json)')[0]
+        except IndexError:
+            return
+
+        if panelDataFileName:
+            with open(panelDataFileName, 'rb') as inputFile:
+                (modpath, data) = json.load(inputFile)
+
+            if (not modpath) or (not data):
+                return
+
+            (modname, classname) = modpath.rsplit('.', 1)
+            try:
+                panelMod = importlib.import_module(modname)
+                klass = getattr(panelMod, classname)
+                tag = getattr(panelMod, 'tags')
+                panel = klass()
+                panel.setPanelData(data)
+
+                self.tabFrame.addTab(panel, tag['top'].name)
+                self.tabFrame.setCurrentWidget(panel)
+            except:
+                QtWidgets.QMessageBox.warning(self, "Invalid module data",
+                    "Module could not be found or panel data invalid",
+                    QtWidgets.QMessageBox.Ok)
 
     def setEnabled(self, state):
         for child in range(self.tabFrame.count()):
