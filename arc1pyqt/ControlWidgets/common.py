ff2e933d64167f1aebcc53ce49eed7794079e39e
Spyros Stathopoulos <ss2y15@soton.ac.uk>
2021-01-29 21:18:38 +0000

    Always clip colorbar values to 1.0
    Older Qt versions do not support the ExtendedRgb colormodel and will
    complain if a value greater than 1.0 is passed to setRgbF. This commit
    clips all colorbar values to a maximum of 1.0

--- arc1pyqt/ControlWidgets/common.py
+++ arc1pyqt/ControlWidgets/common.py
@@ -10,10 +10,14 @@ def _rainbow(x=0):
     elif x < 0:
         x = 0
 
+    r = np.abs(2.0 * x/255.0 - 0.5)
+    g = np.sin(x/255.0 * np.pi)
+    b = np.cos(x/255.0 * np.pi/2)
+
     return (
-        np.abs(2.0 * x/255.0 - 0.5),
-        np.sin(x/255.0 * np.pi),
-        np.cos(x/255.0 * np.pi/2))
+        1.0 if r > 1.0 else r,
+        1.0 if g > 1.0 else g,
+        1.0 if b > 1.0 else b)
 
 for i in range(256):
     color = QColor()
