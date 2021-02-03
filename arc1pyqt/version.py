8323c03247fab2c9230d26762eb6acdbb8a82798
Spyros Stathopoulos <ss2y15@soton.ac.uk>
2021-02-03 00:51:13 +0000

    Update version checking mechanism

--- arc1pyqt/version.py
+++ arc1pyqt/version.py
@@ -1,6 +1,10 @@
+import sys
 import os.path
 import requests
+from requests.exceptions import HTTPError, ConnectionError, Timeout
 import semver
+import json
+from json.decoder import JSONDecodeError
 
 
 def vercmp(local, remote):
@@ -17,13 +21,28 @@ class VersionInfo:
 
     @property
     def remote(self):
-        url = "http://files.arc-instruments.co.uk/release/version.txt"
+        url = 'https://api.github.com/repos/arc-instruments/arc1_pyqt/releases/latest'
         response = requests.get(url, stream=True, timeout=2)
 
         if response.status_code >= 400:
-            raise Exception("Invalid response from server")
+            raise HTTPError("Invalid response from version server")
 
-        return str(response.text.split("\n")[1])
+        ver = str(json.loads(response.content)['tag_name'])
+        return ver
 
     def update_available(self):
-        return vercmp(self.local, self.remote) > 0
+        try:
+            return vercmp(self.local, self.remote) > 0
+        except HTTPError as httperr:
+            print("HTTP Error received from version server:", httperr, file=sys.stderr)
+            return False
+        except (Timeout, ConnectionError) as cerr:
+            print("Error when connecting to version server:", cerr, file=sys.stderr)
+            return False
+        except (JSONDecodeError, IndexError) as jserr:
+            print("Could not parse version server response:", jserr, file=sys.stderr)
+            return False
+        except Exception as exc:
+            print("Unknown exception while connecting to version server:",
+                type(exc), exc, file=sys.stderr)
+            return False
